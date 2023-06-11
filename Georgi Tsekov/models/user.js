b153fd7858be87687b3c2cbb952bcab56be45b97
Georgi Tsekov <72854235+Georgi1212@users.noreply.github.com>
2023-06-11 19:24:15 +0300

    Add files via upload

---
+++ Georgi Tsekov/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    
+    username:{
+        type:String,
+        maxlength:[50, 'username cannot exceed 50 characters'],
+        required: [true,'username is a required field'],
+        unique: [true,'username is a unique field']
+    },
+    firstName:{
+        type:String,
+        maxlength:[50, 'firstName cannot exceed 50 characters'],
+        required: [true,'firstName is a required field']
+    },
+    lastName:{
+        type:String,
+        maxlength:[50, 'lastName cannot exceed 50 characters'],
+        required: [true,'lastName is a required field']
+    },
+    email:{
+        type:String,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/,'Invalid email format'],
+        required:[true,'email is a required field'],
+        unique:[true, 'email is a unique field']
+    },
+    password:{
+        type:String,
+        minlength:[8, 'password must be at least 8 characters long'],
+        required: [true,'password is a required field'],
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+});
+
+const model = mongoose.model('User', userSchema);
+
+module.exports = model;
