afb6f15bf238cdbb36011502a22c7426cd48c9be
Georgi Tsekov <72854235+Georgi1212@users.noreply.github.com>
2023-06-11 19:22:08 +0300

    Delete Georgi Tsekov directory

--- Georgi Tsekov/views/script/addTicketPopup.js
+++
@@ -1,8 +0,0 @@
-function openAddTicketForm() {
-    document.getElementById("add-ticket-popup").style.display = "block";
-}
-
-
-function closeForm() {
-    document.getElementById("add-ticket-popup").style.display = "none";
-}
